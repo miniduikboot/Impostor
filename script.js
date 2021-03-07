61ef36837a9740a066ec3e89fa50577fa1095ad6
miniduikboot <5243971+miniduikboot@users.noreply.github.com>
2021-03-07 22:54:36 +0100

    Convert site to use the new JSON format
    Notable changes:
    - Custom ports are not supported by the current game version, so the
      port selection is commented out.
    - Currently every game needs their own file, so there's a platform
      selector and a set of keys for each platform.
    - Replace every occurrence of .dat by .json

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-var REGION_NAME = "Impostor"
 var SERVER_PORT = 22023;
 
-$(document).ready(function(){
+var DEFAULT_KEYS = {
+    // Version 2021.3.5s
+    steam: { selected: 'NHKLLGFLCLM', servers: 'PBKMLNEHKHL' },
+    // Version 2021.3.5a
+    android: { selected: 'PFJBDMNLKOC', servers: 'LIDGKFEKEMH' },
+    // Version 2021.3.5i
+    itch: { selected: 'FMCHICBDPCE', servers: 'HACEEIOGMDC' },
+    // Version 2021.3.5e
+    epic: { selected: 'KCEOFOCILEP', servers: 'FHHFNNEMCJC' },
+    // Version 2021.2.21m
+    win10: { selected: 'BIPBMPFJBMI', servers: 'GECPHDGCFJM' },
+}
+
+$(document).ready(function() {
     fillIPAdressUsingLocationHash();
-    
+
     showPlatformText();
-    
-    $("#serverFileForm").submit(function(e){
+
+    $("#serverFileForm").submit(function(e) {
         e.preventDefault();
         let serverIp = $("#ip").val();
-        let serverPort = $("#port").val();
-        let serverFileBytes = generateServerFile(REGION_NAME, serverIp, serverPort);
+        // Ports cannot be changed in the current version
+        //let serverPort = $("#port").val();
+        let serverFqdn = $("#fqdn").val();
+        let serverName = $("#name").val();
+        if (serverName == "") {
+            serverName = "Impostor";
+        }
+        let platform = $("#keys-radio input[type='radio']:checked").attr("value")
+        let keys = DEFAULT_KEYS[platform];
+        let serverFileBytes = generateServerFile(serverName, serverIp, serverFqdn, keys);
         let blob = new Blob([serverFileBytes.buffer]);
-        saveFile(blob, "regionInfo.dat");
+        saveFile(blob, "regionInfo.json");
     });
 
 });
@@ -22,7 +42,7 @@ function fillIPAdressUsingLocationHash() {
     let serverIp = urlServerAddress[0];
     let serverPort = urlServerAddress.length > 1 ? urlServerAddress[1] : SERVER_PORT.toString();
     const ipPattern = $("#ip").attr("pattern");
-    
+
     if (new RegExp(ipPattern).test(serverIp)) {
         $("#ip").val(serverIp);
     }
@@ -36,39 +56,37 @@ function showPlatformText() {
         $('.ios-support').show();
     } else if (navigator.userAgent.match(/android/i)) {
         $('.android-support').show();
+        $("#keys-android").prop("checked", true)
     } else {
         $('.desktop-support').show();
+        $("#keys-steam").prop("checked", true)
     }
 }
 
-function generateServerFile(regionName, ip, port) {
-    let bytesArray = int32(0);
-    bytesArray.push(regionName.length);
-    bytesArray = concatArrays(bytesArray, stringToBytes(regionName));
-    bytesArray.push(ip.length);
-    bytesArray = concatArrays(bytesArray, stringToBytes(ip));
-    bytesArray = concatArrays(bytesArray, int32(1));
-    
-    let serverName = regionName + "-Master-1";
+function generateServerFile(name, ip, fqdn, keys) {
+    let server = new Map();
+    server['$type'] = 'DnsRegionInfo, Assembly-CSharp';
+    server['Fqdn'] = fqdn;
+    server['DefaultIp'] = ip;
+    server['Name'] = name;
+    server['TranslateName'] = 1003;
 
-    bytesArray.push(serverName.length);
-    bytesArray = concatArrays(bytesArray, stringToBytes(serverName));
-    bytesArray = concatArrays(bytesArray, ipAddressToBytes(ip));
-    bytesArray = concatArrays(bytesArray, int16(port));
-    bytesArray = concatArrays(bytesArray, int32(0));
-    return Uint8Array.from(bytesArray);
+    let file = new Map();
+    file[keys.selected] = 0;
+    file[keys.servers] = [server];
+    return new Uint8Array(stringToBytes(JSON.stringify(file)));
 }
 
 function saveFile(blob, fileName) {
     let url = URL.createObjectURL(blob);
-    
+
     let a = document.createElement("a");
     document.body.appendChild(a);
     a.style = "display: none";
     a.href = url;
     a.download = fileName;
     a.click();
-    
+
     URL.revokeObjectURL(url);
 }
 
@@ -78,38 +96,4 @@ function stringToBytes(str) {
         bytes.push(str.charCodeAt(i));
     }
     return bytes;
-}
-
-function int16(int) {
-    //Convert integer number to little-endian 16-bits int representation
-    return [(int & 0xFF), 
-            (int & 0xFF00) >> 8];
-}
-
-function int32(int) {
-    //Convert integer number to little-endian 32-bits int representation
-    return [(int & 0xFF), 
-            (int & 0xFF00) >> 8, 
-            (int & 0xFF0000) >> 16, 
-            (int & 0xFF000000) >> 24];
-}
-
-function ipAddressToBytes(ipAddress) {
-    let octets = ipAddress.split(".");
-    let bytes = [];
-    for(let i = 0; i < octets.length; i++) {
-        bytes.push(parseInt(octets[i]));
-    }
-    return bytes;
-}
-
-function concatArrays(array1, array2) {
-    let newArray = [];
-    for (let i = 0; i < array1.length; i++) {
-        newArray.push(array1[i]);
-    }
-    for (let i = 0; i < array2.length; i++) {
-        newArray.push(array2[i]);
-    }
-    return newArray;
 }
